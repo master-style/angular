b11aec6d469342885a0cfae55f6d42afa083464c
Aron <aron886tw@gmail.com>
2021-04-07 12:29:19 +0800

    add test project

---
+++ projects/test/src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    constructor() { }
+
+    timer;
+
+    options = [];
+
+    cities = ['taipei', 'hualien'];
+
+    cityEntities = { id: 'taipei' }
+
+    optionalCities;
+
+    ngOnInit(): void {
+        let i = 0;
+        this.timer = setInterval(() => {
+            if (i > 5) return;
+            i++;
+            this.options.unshift(new Date());
+        }, 1000);
+
+        setTimeout(() => {
+            this.optionalCities = ['taichung', 'kaosiung', 'taipei', 'hualien', 'taoyuan']
+        }, 1000);
+    }
+
+    submit(form) {
+        if (!form.valid) return console.log('invalid');
+        console.log('submit', form);
+    }
+
+    addOption(event) {
+        console.log(event.data);
+    }
+
+    masterChange(event) {
+        // console.log(event, event.target.value);
+    }
+
+    nativeChange(event) {
+        // console.log(event, event.target.value);
+    }
+
+    ngOnDestroy(): void {
+        clearInterval(this.timer);
+    }
+
+}
